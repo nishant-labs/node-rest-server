514d435c5908ca3a8176b044be725f811cc2159b
Nishant Kumar Sinha <nishantsinha-5@users.noreply.github.com>
2018-08-26 21:14:41 +0100

    basic setup with webapck and babel for es6 usage

---
+++ tools/webpack.config.js
@@ -0,0 +1,35 @@
+var path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+	mode: "development",
+	target: "node",
+	devtool: "source-map",
+	externals: [nodeExternals()],
+	entry: path.join(process.cwd(), "src/index.js"),
+	output: {
+		path: path.join(process.cwd(), "dist"),
+		filename: "index.js",
+		library: "node-rest-api",
+		libraryTarget: "umd",
+		umdNamedDefine: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.js$/,
+				loader: "eslint-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	node: {
+		__dirname: false,
+		__filename: false
+	}
+};
