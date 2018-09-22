fd06b0e67e9abbd18e0acb44c5abf54eb0ad69d0
Nishant Kumar Sinha <nishantsinha-5@users.noreply.github.com>
2018-09-22 17:20:42 +0100

    webpack config for prod and dev build

--- configuration/webpack.shared.js
+++ configuration/webpack.shared.js
@@ -1,16 +1,8 @@
-var path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	target: 'node',
 	externals: [nodeExternals()],
-	entry: path.join(process.cwd(), 'src/index.js'),
-	output: {
-		path: path.join(process.cwd(), 'lib'),
-		filename: 'index.js',
-		library: 'node-rest-server',
-		libraryTarget: 'commonjs2',
-	},
 	module: {
 		rules: [
 			{
