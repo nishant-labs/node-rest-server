e202dd8f3bfa3c6987fa5f5439940e98cbc7ce9f
Nishant Kumar Sinha <nishantsinha-5@users.noreply.github.com>
2020-07-18 17:56:18 +0100

    task: setup rollup and upgrade dependencies along with proper lint and formatting hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
 	env: {
 		es6: true,
-		node: true
+		node: true,
 	},
 	extends: 'eslint:recommended',
 	parser: 'babel-eslint',
 	parserOptions: {
 		ecmaVersion: 2015,
-		sourceType: 'module'
+		sourceType: 'module',
 	},
 	rules: {
 		indent: ['error', 'tab'],
 		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', "single", { "avoidEscape": true }],
+		quotes: ['error', 'single', { 'avoidEscape': true }],
 		semi: ['error', 'always'],
-		'comma-dangle': ['error', 'always-multiline']
-	}
+		'comma-dangle': ['error', 'always-multiline'],
+	},
 };
