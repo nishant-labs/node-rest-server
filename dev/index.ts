8e7ce3b3a02c71a53e3532605d63c90b8f5351cd
Nishant Kumar Sinha <4038289+nishant-labs@users.noreply.github.com>
2024-09-21 22:01:37 +0100

    feat: fix vulnerabilities

--- dev/index.ts
+++ dev/index.ts
@@ -1,19 +1,21 @@
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 
-const getFile = (file: string, timeout: number) => {
+const getFile = async (file: string, timeout: number) => {
 	const exists = existsSync(file);
 	if (exists) {
-		import('./server');
+		await import('./server');
 	} else {
-		const stopTimer = setInterval(function () {
+		const stopTimer = setInterval(() => {
 			const fileExists = existsSync(file);
 			if (fileExists) {
 				clearInterval(stopTimer);
-				import('./server');
+				void (async () => {
+					await import('./server');
+				})();
 			}
 		}, timeout);
 	}
 };
 
-getFile(join(process.cwd(), '/lib/index.mjs'), 1000);
+await getFile(join(process.cwd(), '/lib/index.mjs'), 1000);
--- src/utils/ServerProcessor.ts
+++ src/utils/ServerProcessor.ts
@@ -2,7 +2,7 @@ import * as http from 'node:http';
 import express, { Express } from 'express';
 import cors from 'cors';
 import { logger } from './Logger';
-import { ServerConfiguration } from '../types/config.types';
+import { RestServer, ServerConfiguration } from '../types/config.types';
 
 const configProcessor = (app: Express, serverConfig: ServerConfiguration) => {
 	app.set('port', serverConfig.port || 8000);
@@ -25,27 +25,25 @@ export const initPreProcessors = (app: Express, serverConfig: ServerConfiguratio
 	registerPreprocessor(app, serverConfig);
 };
 
-export const getServerReturnHandlers = (server?: http.Server) => {
-	return {
-		close: (forced?: boolean) =>
-			new Promise<Error | undefined>((resolve): void => {
-				if (!server) {
-					resolve(new Error('Server instance not found'));
-					return;
-				}
-
-				if (forced) {
-					server.closeIdleConnections();
-					server.closeAllConnections();
-				}
-				server.close(resolve);
-			}),
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		addListener: (event: string, listener: (...args: any[]) => void) => {
+export const getServerReturnHandlers = (server?: http.Server): RestServer => ({
+	close: (forced?: boolean) =>
+		new Promise<Error | undefined>((resolve): void => {
 			if (!server) {
+				resolve(new Error('Server instance not found'));
 				return;
 			}
-			return server.addListener(event, listener);
-		},
-	};
-};
+
+			if (forced) {
+				server.closeIdleConnections();
+				server.closeAllConnections();
+			}
+			server.close(resolve);
+		}),
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	addListener: (event: string, listener: (...args: any[]) => void) => {
+		if (!server) {
+			return;
+		}
+		return server.addListener(event, listener);
+	},
+});
